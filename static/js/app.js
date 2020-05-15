2adce9613c03bec462ed89ce9cd023c1fb1126c5
Henry Le <henryle.b11@gmail.com>
2020-05-15 11:53:54 -0500

    writing JS

--- static/js/app.js
+++ static/js/app.js
@@ -1,51 +1,30 @@
-d3.json('data/samples.json').then(data => console.log(data));
+var id;
+var value;
+var text;
 
-    // Grab values from the response json object to build the plots
-    // {var name = data.dataset.name;
-    // var stock = data.dataset.dataset_code;
-    // var startDate = data.dataset.start_date;
-    // var endDate = data.dataset.end_date;
-    // var dates = unpack(data.dataset.data, 0);
-    // var openingPrices = unpack(data.dataset.data, 1);
-    // var highPrices = unpack(data.dataset.data, 2);
-    // var lowPrices = unpack(data.dataset.data, 3);
-    // var closingPrices = unpack(data.dataset.data, 4);
+d3.json('data/samples.json').then(function(data) {
+    // console.log(data);
+    var name = data.name;
+    var id = data.samples.map(row => row.otu_ids);
+    console.log(id)
+    var value = data.samples.sample_values;
+    var text = data.samples.otu_labels;
 
-    // var trace1 = {
-    //   type: "scatter",
-    //   mode: "lines",
-    //   name: name,
-    //   x: dates,
-    //   y: closingPrices,
-    //   line: {
-    //     color: "#17BECF"
-    //   }
-    // };
 
-//     // Candlestick Trace
-//     var trace2 = {
-//       type: "candlestick",
-//       x: dates,
-//       high: highPrices,
-//       low: lowPrices,
-//       open: openingPrices,
-//       close: closingPrices
-//     };
+    var trace1 = {
+      type: "bar",
+      x: id,
+      y: value,
+      text: text
+    };
 
-//     var data = [trace1, trace2];
 
-//     var layout = {
-//       title: `${stock} closing prices`,
-//       xaxis: {
-//         range: [startDate, endDate],
-//         type: "date"
-//       },
-//       yaxis: {
-//         autorange: true,
-//         type: "linear"
-//       }
-//     };
+    var data = [trace1];
 
-//     Plotly.newPlot("plot04", data, layout);
-//   });
-// }
+    var layout = {
+      title: "Belly Button"
+      };    
+
+    Plotly.newPlot("bar", data, layout);
+  });
+
