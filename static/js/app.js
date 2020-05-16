a5bca4fd78d7849c0dc31ac2716828209bd8e316
Henry Le <henryle.b11@gmail.com>
2020-05-16 18:59:38 -0500

    writing bble chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,17 @@
 // d3 promise for json
-const promise = d3.json('data/samples.json')
+promise = d3.json('data/samples.json');
 
 //create an array of all subject IDs 
 promise.then(data => {
-    subjID = data.names;
+    var subjID = data.names;
+    var samples = data.samples;
     // console.log("this is subID", subjID);
     d3.select("select").selectAll("option")
     .data(subjID)
     .enter() // creates placeholder for new data
     .append("option") // appends a option to placeholder
     .text(function (idNum) {return idNum});
-    });
-
-
-
-
+});
 
 // ====================== BAR CHART =====================    
 // function to load initial subjID 
@@ -26,39 +23,32 @@ function init() {
     otuID = promise.then(data => {
         // get subject name
         var otuName = data.names[0];
-        // console.log("this is subjName", otuName);
         // get OTU IDs
-        var otuID = data.samples[0].otu_ids.map(eaID => "OTU " + eaID);
-        // console.log("this is otuID", otuID);
+        var otuID = data.samples[0].otu_ids;
         // OTU Values 
         var otuVal = data.samples[0].sample_values;
-        // console.log("this is otuVal", otuVal);
         // OTU Hover-over Text
         var otuText = data.samples[0].otu_labels;
-        // console.log("this is otuText", otuText);
 
         // extract the demographic info
-        demInfoBox.html("")
+        demInfoBox.html("");
         var demInfo = data.metadata.filter(row => row.id == otuName)[0];
         Object.entries(demInfo).forEach(([key, value]) => {
-            console.log("this is key and value :: ", key, value);
             demInfoBox.append("p").text(`${key}: ${value}`);
         });
-        console.log("this is filtered metadata :: ", demInfo);
     
-        // create trace
-        var trace1 = {
+        // create traceBarBar
+        var traceBar = {
         type: "bar",
         x: otuVal.slice(0,10), // slice first 10 data
-        y: otuID.slice(0,10), // slice first 10 data
+        y: otuID.slice(0,10).map(eaID => "OTU " + eaID), // slice first 10 data
         text: otuText.slice(0,10), // slice first 10 data
         orientation: 'h',  // horizontal bar chart
         name: 'OTU vs. Sample Values'
         };
-        console.log("this is X in trace 1", trace1.x);
-        console.log("this is Y in trace 1", trace1.y);
-        // point plot's data to created trace 
-        var data = [trace1];
+
+        // point plot's data to created traceBar 
+        var data = [traceBar];
     
         // specify layout format parameters
         var layout = {
@@ -68,7 +58,8 @@ function init() {
                     text: "Sample Values"
                 },
                 showgrid : true, // major grid lines 
-                showline: true   // axis line
+                showline: true,   // axis line
+                rangemode : "tozero"
             },
             yaxis: {
                 title: {
@@ -76,66 +67,106 @@ function init() {
                 },  
                 autorange: "reversed",
                 showgrid : true,   // major grid lines
-                showline: true    // axis line
+                showline: true,    // axis line
+                rangemode : "tozero"
             }
         };    
     
         // create plot with tag id=bar (from index.HTML)  
         Plotly.newPlot("bar_plot", data, layout);
+        
+        
+        // ====================== BUBBLE CHART =====================
+        
+
+        // Use sample_values for the y values.
+        var traceBble = {
+            // Use otu_ids for the x values.
+            x : otuID,
+            y : otuVal,
+            marker : {
+                // Use otu_ids for the marker colors.
+                // color: ['red', 'blue', 'green'],
+
+                 // Use sample_values for the marker size.
+                size: otuVal
+            },
+            mode :'markers',
+
+            // Use otu_labels for the text values.
+            text: otuText 
+        };
+
+        
+        var dataBble = [traceBble];
+
+        var layoutBble = {
+            title: 'Bubble Chart',
+            showlegend: false,
+            height: 600,
+            width: 1000,
+            xaxis: {
+                title: {
+                    text: "Sample Values"
+                },
+                showgrid : true, // major grid lines 
+                showline: true   // axis line
+            },
+            yaxis: {
+                title: {
+                    text: "Sample Values"
+                },  
+                showgrid : true,   // major grid lines
+                showline: true,    // axis line
+                rangemode : "tozero"
+            }
+        };
+
+        Plotly.newPlot("bubble", dataBble, layoutBble);      
     })
 }
-
 d3.selectAll("#selDataset").on("change", updatePlotly);
 
 
 function updatePlotly () {
     d3.event.preventDefault();
-    console.log("updatePlotly called");
  
     var dropdownMenu = d3.select("#selDataset");
 
     // Assign the value of the dropdown menu option to a variable
     var currSubID = dropdownMenu.property("value");
-    console.log("the data set selected is :: ", currSubID);
     
     // filter data based on user selection of "Test Subject ID No."
     promise.then(data => {    
 
         // extract the otu_ids array from the json
         var subjArr = data.samples.filter(row => row.id == currSubID);
-        console.log("this is filtered samples :: ", subjArr);
 
         // extract metadata and make a box
         demInfoBox.html("")
         demInfo = data.metadata.filter(row => row.id == currSubID)[0];
         Object.entries(demInfo).forEach(([key, value]) => {
-            console.log("this is key and value :: ", key, value);
             demInfoBox.append("p").text(`${key}: ${value}`);
         });
-        console.log("this is filtered metadata :: ", demInfo);
 
         // get OTU IDs
         var otuID = subjArr[0].otu_ids;
-        otuID = otuID.map(eaID => "OTU " + eaID);
-        console.log("this is otuID", otuID);
+        otuID = otuID.map(eaID => `OTU ${eaID}`);
 
         // OTU Values 
         var otuVal = subjArr[0].sample_values;
-        console.log("this is otuVal", otuVal);
 
         // OTU Hover-over Text
         var otuText = subjArr[0].otu_labels;
-        console.log("this is otuText", otuText);
 
+
+        // ====================== UPDATE BAR CHART ========================
         // specify what needs to be updated
         var update = {
             text : otuText.slice(0,10),  // take only top 10 data points 
             x: [otuVal.slice(0,10)],
             y: [otuID.slice(0,10)]
         };
-
-        console.log("this is new trace X :: ", update.x, "\n"
-            )
 
         // specify new graph title
         var reLayout = {
@@ -145,6 +176,15 @@ function updatePlotly () {
         // update plot
         Plotly.restyle("bar_plot", update);
         Plotly.relayout("bar_plot", reLayout)
+
+
+    // ====================== BUBBLE CHART =====================
+
+
+        
+  
+
+    
     })  
 }
 
@@ -153,19 +193,6 @@ function updatePlotly () {
 console.info("attempting event handling");
 console.log(d3.select("#selDataset")); 
 
-init()
-
-// ====================== BUBBLE CHART =====================
-// Use otu_ids for the x values.
 
 
-// Use sample_values for the y values.
 
-
-// Use sample_values for the marker size.
-
-
-// Use otu_ids for the marker colors.
-
-
-// Use otu_labels for the text values.
